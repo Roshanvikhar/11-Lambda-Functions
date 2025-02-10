9d2582763adc72f453c7a8aa6246e296d726e10b
Roshanvikhar <roshanvikhar123@gmail.com>
2025-02-10 13:11:59 +0530

    Add reusable workflow and Java function deployment

---
+++ test_lambda.py
@@ -0,0 +1,8 @@
+# test_lambda.py
+import lambda_function
+
+def test_lambda_handler():
+    event = {}
+    context = {}
+    response = lambda_function.lambda_handler(event, context)
+    assert response['statusCode'] == 200
