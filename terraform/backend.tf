3c426258a2fa4e7532104af52f200046357816db
roshan vikhar <roshanvikhar@roshans-MacBook-Pro.local>
2025-04-27 23:35:53 +0530

    adding terraform s3 integration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket         = "github-actions-terraform-test-tfstate"  # Your S3 bucket for storing state
-    key            = "terraform/state.tfstate"              # Path within the bucket to store the state file
-    region         = "us-east-1"                            # AWS region for the S3 bucket
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "github-actions-terraform-test-tfstate"  # Your S3 bucket for storing state
+#     key            = "terraform/state.tfstate"              # Path within the bucket to store the state file
+#     region         = "us-east-1"                            # AWS region for the S3 bucket
+#   }
+# }
