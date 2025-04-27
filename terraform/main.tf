3c83283c68e794c774a87b4e1ca25a01c217ac81
roshan vikhar <roshanvikhar@roshans-MacBook-Pro.local>
2025-04-27 22:13:03 +0530

    adding terraform s3 integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Use existing bucket instead of creating
-data "aws_s3_bucket" "github_bucket" {
+# ✅ Create bucket (because it does not exist)
+resource "aws_s3_bucket" "github_bucket" {
   bucket = "github-actions-terraform-test"
 }
 
-# Upload test file into existing bucket
+# ✅ Upload test file into the created bucket
 resource "aws_s3_bucket_object" "uploaded_file" {
-  bucket = data.aws_s3_bucket.github_bucket.id
+  bucket = aws_s3_bucket.github_bucket.id
   key    = "test-file.txt"
   source = "test-file.txt"
   etag   = filemd5("test-file.txt")
 }
 
 output "bucket_name" {
-  value = data.aws_s3_bucket.github_bucket.id
+  value = aws_s3_bucket.github_bucket.id
 }
