beb567deabdb2c8968d68c94e7bb096e5aec46ed
roshan vikhar <roshanvikhar@roshans-MacBook-Pro.local>
2025-04-27 20:56:56 +0530

    adding terraform s3 integration

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "github-actions-terraform-test"  # Your S3 bucket for storing state
+    key            = "terraform/state.tfstate"              # Path within the bucket to store the state file
+    region         = "us-east-1"                            # AWS region for the S3 bucket
+    encrypt        = true                                  # Enable encryption for the state file
+    versioning     = true                                  # Enable versioning on the S3 bucket to store previous versions of state
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "github_bucket" {
+  bucket = "github-actions-terraform-test"
+}
+
+resource "aws_s3_bucket_object" "uploaded_file" {
+  bucket = aws_s3_bucket.github_bucket.id
+  key    = "test-file.txt"
+  source = "test-file.txt" # This file must exist locally
+  etag   = filemd5("test-file.txt")
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.github_bucket.id
+}
