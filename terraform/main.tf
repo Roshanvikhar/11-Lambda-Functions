935003bb2aa99c83490bc52d61b8dd3cdc4054fe
Roshan Narendra Vikhar <roshanvi@cybage.com>
2025-06-16 13:28:35 +0530

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,14 @@ resource "aws_s3_bucket_object" "uploaded_file" {
 output "bucket_name" {
   value = aws_s3_bucket.github_bucket.id
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "github-actions-terraform-test-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
